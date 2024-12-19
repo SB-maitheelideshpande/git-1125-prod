3cf5b881d4182bfe6d9c3750e614777d16263e81
maitheeli.deshpande@smartbear.com <maitheeli.deshpande@smartbear.com>
2024-12-19 12:17:58 +0530

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2NzU5NA/spectral.js?branch_slug=main&token=ab77b7f3-b973-45e0-aa75-2bcbdabfff5f";
+export default { extends: ruleset };
